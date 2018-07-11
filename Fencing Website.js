294ac99a0c8534765b5b7e8914e4df04e8320c03
D3Lphin3 <41123990+D3Lphin3@users.noreply.github.com>
2018-07-11 11:08:35 -0700

    Create Fencing Website.js

---
+++ Fencing Website.js
@@ -0,0 +1,15 @@
+function description(){
+  document.getElementById('intro').innerHTML = url("https://www.youtube.com/watch?v=dRGQ1agYpYU");
+}
+
+function foilfencing(){
+  document.getElementById('ffoil').innerHTML = url("https://www.youtube.com/watch?v=TZSOZF1NF3Q");
+}
+
+function sabrefencing(){
+  document.getElementById('fsabre').innerHTML = url("https://www.youtube.com/watch?v=tG4z1vyolck");
+}
+
+function epeefencing(){
+  document.getElementById('fepee').innerHTML = url("https://www.youtube.com/watch?v=vxlz8IqvkcU");
+}
